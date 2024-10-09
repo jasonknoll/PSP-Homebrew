1bdda009f9fa8d6172b31b5e2430723ac3825f2e
Jason Knoll <jasonknoll98@aol.com>
2024-10-08 21:35:45 -0400

    Update main.c

--- PSPGL-Test/main.c
+++ PSPGL-Test/main.c
@@ -9,3 +9,10 @@
 // TODO compare PSPGL and PSPGU
 // draw a cube in this and PSPGU
 PSP_MODULE_INFO("pspGL Test", 0, 1, 0);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_VFPU | THREAD_ATTR_USER);
+
+int exit_callback() {}
+
+int callback_thread() {}
+
+int setup_callbacks(void) {}
