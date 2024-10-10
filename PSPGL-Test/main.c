353d0e689ee15ed80ef1a6c57d9520dda174756b
Jason Knoll <jasonknoll98@aol.com>
2024-10-09 21:47:01 -0400

    Update main.c

--- PSPGL-Test/main.c
+++ PSPGL-Test/main.c
@@ -6,6 +6,10 @@
 // #include <pspgu.h>
 // #include <pspgum.h>
 
+#define GREEN 0xff00ff00
+#define BLUE 0xffff0000
+
+
 // TODO compare PSPGL and PSPGU
 // draw a cube in this and PSPGU
 PSP_MODULE_INFO("pspGL Test", 0, 1, 0);
@@ -16,3 +20,11 @@ int exit_callback() {}
 int callback_thread() {}
 
 int setup_callbacks(void) {}
+
+int main() {
+    setup_callbacks();
+
+    for (;;) {
+        // main loop
+    }
+}
