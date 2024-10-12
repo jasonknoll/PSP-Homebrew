ba1397f912519361e611a6739f501a62096430f8
Jason Knoll <jasonknoll98@aol.com>
2024-10-12 13:05:21 -0400

    Trying to port the rectangle example to rust-psp

--- psp-rs/src/main.rs
+++ psp-rs/src/main.rs
@@ -2,9 +2,10 @@
 #![no_main]
 
 use core::ffi::c_void;
-use psp::sys::{self, DisplayPixelFormat, GuState, TexturePixelFormat};
+use psp::sys::{self, sceGuDisplay, sceGuEnable, SceCtrlData, ClearBuffer, DisplayPixelFormat, GuContextType, GuState, GuSyncBehavior, GuSyncMode, TexturePixelFormat, CtrlMode};
 use psp::vram_alloc::get_vram_allocator;
 use psp::{BUF_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH};
+use psp::sys::DepthFunc::Equal;
 
 psp::module!("sample_module", 1, 0);
 
@@ -20,6 +21,28 @@ static mut LIST: psp::Align16<[u32; 0x40000]> = psp::Align16([0; 0x40000]);
 fn psp_main() {
     psp::enable_home_button();
 
+
+
+    // spooky
+    unsafe {
+        init_gu();
+
+        let pad = SceCtrlData::default(); // controller
+
+        sys::sceCtrlSetSamplingCycle(0);
+        sys::sceCtrlSetSamplingMode(CtrlMode::Analog);
+
+        loop {
+            start_frame();
+
+            todo!("Need to draw rectangle vertices and control it with d-pad");
+
+            end_frame();
+        }
+    }
+}
+
+unsafe fn init_gu() {
     let mut allocator = get_vram_allocator().unwrap();
     let fbp0 = allocator
         .alloc_texture_pixels(BUF_WIDTH, SCREEN_HEIGHT, TexturePixelFormat::Psm8888)
@@ -31,9 +54,46 @@ fn psp_main() {
         .alloc_texture_pixels(BUF_WIDTH, SCREEN_HEIGHT, TexturePixelFormat::Psm4444)
         .as_mut_ptr_from_zero();
 
-    // spooky
-    unsafe {
-        sys::sceGuInit();
-        sys::sceGuStart(sys::GuContextType::Direct, LIST);
-    }
+    sys::sceGuInit();
+    sys::sceGuStart(
+        sys::GuContextType::Direct,
+        &mut LIST as *mut _ as *mut c_void,
+    );
+    sys::sceGuDrawBuffer(DisplayPixelFormat::Psm8888, fbp0 as _, BUF_WIDTH as i32);
+    sys::sceGuDispBuffer(SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32, fbp1 as _, BUF_WIDTH as i32);
+    sys::sceGuDepthBuffer(zbp as _, BUF_WIDTH as i32);
+
+    sys::sceGuOffset(2048 - (SCREEN_WIDTH/2), 2048 - (SCREEN_HEIGHT/2));
+    sys::sceGuViewport(2048, 2048, SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32);
+    sceGuEnable(GuState::ScissorTest);
+    sys::sceGuScissor(0, 0, SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32);
+
+    sys::sceGuDepthRange(65535, 0);
+
+    sys::sceGuDepthFunc(Equal);
+    sys::sceGuEnable(GuState::DepthTest);
+
+    sys::sceGuFinish();
+    sceGuDisplay(true);
+}
+
+unsafe fn start_frame() {
+    sys::sceGuStart(GuContextType::Direct, &mut LIST as *mut _ as *mut c_void);
+    sys::sceGuClearColor(0xffffffff);
+    sys::sceGuClear(ClearBuffer::COLOR_BUFFER_BIT);
+}
+
+unsafe fn end_frame() {
+    sys::sceGuFinish();
+    sys::sceGuSync(GuSyncMode::Finish, GuSyncBehavior::Wait);
+    sys::sceDisplayWaitVblankStart();
+    sys::sceGuSwapBuffers();
+}
+
+unsafe fn draw_rect() {
+    todo!()
+}
+
+unsafe fn draw_cube() {
+    todo!()
 }
